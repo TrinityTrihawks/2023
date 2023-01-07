dd7c1d5f7396a195e4f8cd07c23a01fc9f128713
Anthony6444 <anthonydc1972@outlook.com>
2023-01-07 09:58:29 -0600

    update package name

--- src/main/java/org/trinity4215/robot2023/RobotContainer.java
+++ src/main/java/org/trinity4215/robot2023/RobotContainer.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.trinity4215.robot2023;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import org.trinity4215.robot2023.Constants.OperatorConstants;
+import org.trinity4215.robot2023.commands.Autos;
+import org.trinity4215.robot2023.commands.ExampleCommand;
+import org.trinity4215.robot2023.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
--- src/main/java/org/trinity4215/robot2023/commands/ExampleCommand.java
+++ src/main/java/org/trinity4215/robot2023/commands/ExampleCommand.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package org.trinity4215.robot2023.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import org.trinity4215.robot2023.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
